ee7cd57567f7251b18e72ece8103b6a4a2801a58
Ricardo Sobral <riki.sobral@gmail.com>
2021-05-06 20:23:56 -0300

    chakra ui input, component input custom

--- semana-04/dashgo/src/pages/index.tsx
+++ semana-04/dashgo/src/pages/index.tsx
@@ -1,5 +1,26 @@
-export default function Home() {
+import { Flex, Button, Stack } from "@chakra-ui/react";
+import { Input } from "../components/Form/Input";
+
+export default function SignIn() {
   return (
-    <h1>Dashgo</h1>
-  )
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxWidth="360px"
+        bg="gray.800"
+        p="8"
+        borderRadius="8"
+        flexDirection="column"
+      >
+        <Stack spacing="4">
+          <Input name="email" label="Email" type="email" />
+          <Input name="password" type="password" label="Senha" />
+          <Button type="submit" marginTop="6" colorScheme="pink" size="lg">
+            Entrar
+          </Button>
+        </Stack>
+      </Flex>
+    </Flex>
+  );
 }
