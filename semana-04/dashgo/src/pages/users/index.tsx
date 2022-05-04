c162167448787cc491a9f261a32dc98316dd86ea
Ricardo-Sobral7 <riki.sobral@gmail.com>
2022-05-04 07:55:43 -0300

    update branch

--- semana-04/dashgo/src/pages/users/index.tsx
+++ semana-04/dashgo/src/pages/users/index.tsx
@@ -1,10 +1,19 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import Link from "next/link";
 import { RiAddLine, RiPencilFill } from "react-icons/ri";
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue(
+    {
+      base: false,
+      lg: true
+    }
+  )
+
   return (
     <Box>
       <Header />
@@ -23,15 +32,17 @@ export default function UserList() {
             align='center'
           >
             <Heading size='lg' fontWeight='normal'>Usuários</Heading>
-            <Button
-              as='a'
-              size='sm'
-              fontSize='sm'
-              colorScheme='pink'
-              leftIcon={<Icon as={RiAddLine} fontSize='18'/>}
-            >
-              Criar novo
-            </Button>
+            <Link href="/users/create" passHref>
+              <Button
+                as='a'
+                size='sm'
+                fontSize='sm'
+                colorScheme='pink'
+                leftIcon={<Icon as={RiAddLine} fontSize='18' />}
+              >
+                Criar novo
+              </Button>
+            </Link>
           </Flex>
 
           <Table colorScheme='whiteAlpha'>
@@ -47,16 +58,18 @@ export default function UserList() {
                 <Th>
                   Usuário
                 </Th>
-                <Th>
-                  Data de cadastro
-                </Th>
+                {isWideVersion &&
+                  <Th>
+                    Data de cadastro
+                  </Th>
+                }
                 <Th width='8' />
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
                 <Td
-                  px='6'
+                  px={['4', '4', '6']}
                 >
                   <Checkbox colorScheme='pink' />
                 </Td>
@@ -66,23 +79,29 @@ export default function UserList() {
                     <Text fontSize='sm' color='gray.300'>ricardo.sobral@gmail.com</Text>
                   </Box>
                 </Td>
+                {isWideVersion &&
+                  <Td>
+                    29 de janeiro, 2022.
+                  </Td>
+                }
                 <Td>
-                  29 de janeiro, 2022.
-                </Td>
-                <Td>
-                  <Button
-                    as='a'
-                    size='sm'
-                    fontSize='sm'
-                    colorScheme='purple'
-                    leftIcon={<Icon as={RiPencilFill} fontSize='16'/>}
-                  >
-                    Editar
-                  </Button>
+                  {isWideVersion &&
+                    <Button
+                      as='a'
+                      size='sm'
+                      fontSize='sm'
+                      colorScheme='purple'
+                      leftIcon={<Icon as={RiPencilFill} fontSize='16' />}
+                      p={['4', '2']}
+                    >
+                      Editar
+                    </Button>
+                  }
                 </Td>
               </Tr>
             </Tbody>
           </Table>
+          <Pagination />
         </Box>
       </Flex>
     </Box>
