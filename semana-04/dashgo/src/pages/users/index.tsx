eae628bec7a0bb3e827ee3c6dc8c5d22bf726d6c
Ricardo-Sobral7 <riki.sobral@gmail.com>
2022-08-19 01:22:01 -0300

    data fatching initial config and usage

--- semana-04/dashgo/src/pages/users/index.tsx
+++ semana-04/dashgo/src/pages/users/index.tsx
@@ -1,12 +1,22 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+
 import { RiAddLine, RiPencilFill } from "react-icons/ri";
+
+import { useQuery } from 'react-query'
+
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json()
+    return data
+  })
+
   const isWideVersion = useBreakpointValue(
     {
       base: false,
@@ -45,63 +55,75 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme='whiteAlpha'>
-            <Thead>
-              <Tr>
-                <Th
-                  px='6'
-                  color='gray.300'
-                  width='8'
-                >
-                  <Checkbox colorScheme='pink' />
-                </Th>
-                <Th>
-                  Usuário
-                </Th>
-                {isWideVersion &&
-                  <Th>
-                    Data de cadastro
-                  </Th>
-                }
-                <Th width='8' />
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td
-                  px={['4', '4', '6']}
-                >
-                  <Checkbox colorScheme='pink' />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>Ricardo Sobral</Text>
-                    <Text fontSize='sm' color='gray.300'>ricardo.sobral@gmail.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion &&
-                  <Td>
-                    29 de janeiro, 2022.
-                  </Td>
-                }
-                <Td>
-                  {isWideVersion &&
-                    <Button
-                      as='a'
-                      size='sm'
-                      fontSize='sm'
-                      colorScheme='purple'
-                      leftIcon={<Icon as={RiPencilFill} fontSize='16' />}
-                      p={['4', '2']}
+          {isLoading ? (
+            <Flex align="center" justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter listagem dos usuários</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme='whiteAlpha'>
+                <Thead>
+                  <Tr>
+                    <Th
+                      px='6'
+                      color='gray.300'
+                      width='8'
                     >
-                      Editar
-                    </Button>
-                  }
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+                      <Checkbox colorScheme='pink' />
+                    </Th>
+                    <Th>
+                      Usuário
+                    </Th>
+                    {isWideVersion &&
+                      <Th>
+                        Data de cadastro
+                      </Th>
+                    }
+                    <Th width='8' />
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td
+                      px={['4', '4', '6']}
+                    >
+                      <Checkbox colorScheme='pink' />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight='bold'>Ricardo Sobral</Text>
+                        <Text fontSize='sm' color='gray.300'>ricardo.sobral@gmail.com</Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion &&
+                      <Td>
+                        29 de janeiro, 2022.
+                      </Td>
+                    }
+                    <Td>
+                      {isWideVersion &&
+                        <Button
+                          as='a'
+                          size='sm'
+                          fontSize='sm'
+                          colorScheme='purple'
+                          leftIcon={<Icon as={RiPencilFill} fontSize='16' />}
+                          p={['4', '2']}
+                        >
+                          Editar
+                        </Button>
+                      }
+                    </Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
